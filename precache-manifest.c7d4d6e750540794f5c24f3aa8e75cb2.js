46f3eacdd3e34bc6caba03adb1d26ed5daa5a3e8
Abdulwahab O. Jimoh <omoniyi.success@gmail.com>
2022-12-21 12:36:37 +0100

    Updates

---
+++ precache-manifest.c7d4d6e750540794f5c24f3aa8e75cb2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0f03843e885faac52af",
+    "url": "/ogunstudentawards/static/css/main.1714042e.chunk.css"
+  },
+  {
+    "revision": "f0f03843e885faac52af",
+    "url": "/ogunstudentawards/static/js/main.a45fd840.chunk.js"
+  },
+  {
+    "revision": "3c7db86389340e2116bb",
+    "url": "/ogunstudentawards/static/js/runtime~main.09badb47.js"
+  },
+  {
+    "revision": "2c303c3c3cb078b9c74e",
+    "url": "/ogunstudentawards/static/css/2.1df73492.chunk.css"
+  },
+  {
+    "revision": "2c303c3c3cb078b9c74e",
+    "url": "/ogunstudentawards/static/js/2.112c07cd.chunk.js"
+  },
+  {
+    "revision": "d79f24b9b8d93f05f714",
+    "url": "/ogunstudentawards/static/js/3.9795b191.chunk.js"
+  },
+  {
+    "revision": "f3d22269d3c48d469a4b5074026f8af9",
+    "url": "/ogunstudentawards/static/media/offer.f3d22269.png"
+  },
+  {
+    "revision": "e683c2b6c794107d682c282818a560c9",
+    "url": "/ogunstudentawards/static/media/income.e683c2b6.png"
+  },
+  {
+    "revision": "14eda3a7db77a195072c676efea73bdf",
+    "url": "/ogunstudentawards/static/media/walkthrough.14eda3a7.png"
+  },
+  {
+    "revision": "50d44a72a694945aa660c4593d7b2523",
+    "url": "/ogunstudentawards/static/media/terms.50d44a72.png"
+  },
+  {
+    "revision": "9cb289b8748411c374345ac9d51db634",
+    "url": "/ogunstudentawards/static/media/door_step.9cb289b8.png"
+  },
+  {
+    "revision": "a8f7100b76a074e0283d77d82c548eda",
+    "url": "/ogunstudentawards/static/media/faq.a8f7100b.png"
+  },
+  {
+    "revision": "ea93fd2614e587bab233078c6822c6a9",
+    "url": "/ogunstudentawards/static/media/icon-.ea93fd26.svg"
+  },
+  {
+    "revision": "6716c861aec37b334775d9ad18c94343",
+    "url": "/ogunstudentawards/static/media/icon+.6716c861.svg"
+  },
+  {
+    "revision": "eb8b9dbf28e03e31e73c78d141a95933",
+    "url": "/ogunstudentawards/static/media/aluta2.eb8b9dbf.jpeg"
+  },
+  {
+    "revision": "c52a0698ad8321391540e479f4719ea4",
+    "url": "/ogunstudentawards/static/media/how1.c52a0698.png"
+  },
+  {
+    "revision": "c9359374bdd3d162e031a9215128a70a",
+    "url": "/ogunstudentawards/static/media/deliver.c9359374.png"
+  },
+  {
+    "revision": "64b963ae3b6ce5acee12ffe41a1e6da9",
+    "url": "/ogunstudentawards/static/media/how.64b963ae.png"
+  },
+  {
+    "revision": "1b63546d9b7334519d17c5e2f76c73a4",
+    "url": "/ogunstudentawards/static/media/checkmark.1b63546d.svg"
+  },
+  {
+    "revision": "7b385bedd5e759ed121d0f7da698bf15",
+    "url": "/ogunstudentawards/static/media/top_footer.7b385bed.png"
+  },
+  {
+    "revision": "01aaa07395419643f88a9ac8c17fc44f",
+    "url": "/ogunstudentawards/static/media/banner_bg.01aaa073.png"
+  },
+  {
+    "revision": "205e9b27608deac8cf0dfb18a9e7ff36",
+    "url": "/ogunstudentawards/index.html"
+  }
+];
